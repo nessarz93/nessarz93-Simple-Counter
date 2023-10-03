a8691e664582ada9f3f3d5594c9b5136cc2a241f
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-10-03 23:25:28 +0000

    Correciones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,31 @@ import React from "react";
 import SecondsCounter from "./seconscounter.jsx";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<>
-			<SecondsCounter/>
-		</>
+		<div className= "row reloj">
+			<div className="col counter">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="col counter">
+				{props.digit6}
+			</div>
+			<div className="col counter">
+				{props.digit5}
+			</div>
+			<div className="col counter">
+				{props.digit4}
+			</div>
+			<div className="col counter">
+				{props.digit3}
+			</div>
+			<div className="col counter">
+				{props.digit2}
+			</div>
+			<div className="col counter">
+				{props.digit1}
+			</div>
+		</div>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,18 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let segundo = 0;
+
+setInterval (()=> {
+    const one = Math.floor((segundo/1) % 10)
+    const two = Math.floor((segundo/10) % 10)
+    const three = Math.floor((segundo/100) % 10)
+    const four = Math.floor((segundo/1000) % 10)
+    const five = Math.floor((segundo/10000) % 10)
+    const six = Math.floor((segundo/100000) % 10)
+    segundo+=1
+    ReactDOM.render(<Home digit1={one} digit2={two} digit3={three} digit4={four} digit5={five} digit6={six} />, document.querySelector("#app"));
+},1000)
+
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
